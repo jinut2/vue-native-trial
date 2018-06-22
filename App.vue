c497adf71a8a0c75e53749f91be6d4a02217e388
jinut2 <jinu.t.92@gmail.com>
2018-06-22 17:14:48 +0530

    tried out text, text-input, list-binding, onbuttonpress.'

--- App.vue
+++ App.vue
@@ -1,6 +1,10 @@
 <template>
   <view class="container">
-    <text class="text-color-primary">{{ message }}</text>
+    <text-input class="text-input-container"
+        v-model="text"
+      />
+    <button v-bind:title="message" v-bind:on-press="handleBtnPress"/> 
+    <text v-bind:key="item" v-for="item in list">{{item}}</text>
     </view>
 </template>
  
@@ -8,20 +12,36 @@
 export default {
   data: function() {
     return {
-      message: "Hello World"
+      text: '',
+      message: 'Welcome!',
+      list:[],
     };
+  },
+  methods: {
+    handleBtnPress: function() {
+      if (this.text != "clear"){
+        alert("Hey! "+ this.text);
+        this.list.push(this.text);
+      } else {
+        this.list = [];
+      }
+      this.text = '';
+    }
   }
 };
 </script>
 
 <style>
 .container {
-  background-color: whitesmoke;
-  align-items: center;
-  justify-content: center;
+  background-color: white;
   flex: 1;
+  margin-top: 30;
+  align-self: auto;
+  align-items: center;
 }
-.text-color-primary {
-  color: blue;
+.text-input-container {
+  width: 300;
+  height: 50;
+  border-color: black;
 }
 </style>
